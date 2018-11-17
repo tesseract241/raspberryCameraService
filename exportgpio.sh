0cd1fd48b70ebea9f856e6c16f2133fe923f2906
tesseract241 <endou.kenji5@gmail.com>
2018-11-17 22:32:54 +0100

    Misunderstood the gpio syntax, fixed it

--- exportgpio.sh
+++ exportgpio.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BUTTON_PIN=18; MOTION_SENSOR_PIN=17
 gpio export BUTTON_PIN in
-gpio export BUTTON_PIN in
-gpio mode input up BUTTON_PIN
-gpio mode input tri MOTION_SENSOR_PIN
+gpio export MOTION_SENSOR_PIN in
+gpio mode BUTTON_PIN up
+gpio mode MOTION_SENSOR_PIN tri
 gpio edge BUTTON_PIN both
 gpio edge MOTION_SENSOR_PIN rising
 exit 0
