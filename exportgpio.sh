836c989d36567fed965efd31ebbb9c96a827b4da
tesseract241 <endou.kenji5@gmail.com>
2018-11-21 17:26:29 +0100

    Changed gpio to use BCM pin numbers for consistency with the rest of the code.

--- exportgpio.sh
+++ exportgpio.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-BUTTON_PIN=1; MOTION_SENSOR_PIN=0
-gpio export BUTTON_PIN in
-gpio export MOTION_SENSOR_PIN in
-gpio mode BUTTON_PIN up
-gpio mode MOTION_SENSOR_PIN tri
-gpio edge BUTTON_PIN both
-gpio edge MOTION_SENSOR_PIN rising
+BUTTON_PIN=27; MOTION_SENSOR_PIN=17
+gpio -g export BUTTON_PIN in
+gpio -g export MOTION_SENSOR_PIN in
+gpio -g mode BUTTON_PIN up
+gpio -g mode MOTION_SENSOR_PIN tri
+gpio -g edge BUTTON_PIN both
+gpio -g edge MOTION_SENSOR_PIN rising
 exit 0
