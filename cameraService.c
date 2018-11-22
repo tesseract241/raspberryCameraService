23abd4bb1722010a7da40d8551db0fac81656462
tesseract241 <endou.kenji5@gmail.com>
2018-11-22 19:38:30 +0100

    An unexpected behaviour of wiringPi interrupts running in another thread fixed.
    Signals under linux are raised to just one thread, which meant that the main thread, under which sigsuspend is waiting, would not be alerted to SIGUSR1 being raised and thus never show the video.  
    On another front, attempting to change the command to take a picture so that it doesn't have to be queued after the video has finished, by instead linking the last frame. Needs to be tested.

--- cameraService.c
+++ cameraService.c
@@ -10,19 +10,24 @@
 #define HOLDING_TIME 2 
 #define VIDEO_LENGTH "5000" // In milliseconds
 
-volatile _Bool take_picture_check = FALSE;
+volatile _Bool link_frame_check 	= FALSE;
+volatile _Bool show_video_check	= FALSE;
 
-volatile _Bool is_button_pressed = FALSE;
+volatile _Bool is_button_pressed 	= FALSE;
 
 
 void alrm_handler(int signum){
-	take_picture_check = TRUE;
+	link_frame_check = TRUE;
+}
+
+void sigusr1_handler(int signum){
+	show_video_check = TRUE;
 }
 
 
 void show_video(){
 	// screenON(); // To be added when I get the display and can write code for it
-	system("raspivid -f -w 480 -h 320 -ex auto -t " VIDEO_LENGTH);
+	system("raspivid -f -w 1920 -h 1080 -ex auto -t " VIDEO_LENGTH);
 	// screenOFF(5); // To be added when I get the display and can write code for it
 }
 
@@ -30,7 +35,15 @@ void take_picture(){
 	time_t t = time(NULL);
 	char filename[15], buf[75];
 	strftime(filename, 15, "%Y%m%d%H%M%S", localtime(&t));
-	sprintf(buf, "raspistill -w 1920 -h 1080 -q 75 -e png -n -t 100 -o %s.png", filename);
+	sprintf(buf, "raspistill -w 1920 -h 1080 -q 75 -e png -l %s.png", filename);
+	system(buf);
+}
+
+void link_frame(){
+	time_t t = time(NULL);
+	char filename[15], buf[75];
+	strftime(filename, 15, "%Y%m%d%H%M%S", localtime(&t));
+	sprintf(buf, "raspistill -w 1920 -h 1080 -q 75 -e png -n -t 300 -o %s.png", filename);
 	system(buf);
 }
 
@@ -39,8 +52,8 @@ void button_click_handler(){
 	is_button_pressed=!is_button_pressed;
 	if(is_button_pressed){
 		alarm(HOLDING_TIME);
-//Even if the alarm goes off while show_video is still running is fine, as we are in a normal context.
-		show_video();
+//The reason to use kill instead of raise is that we are in a different thread, so raise would happen  in a different thread from the one where sigsuspend is running.
+		kill(getpid(), SIGUSR1);
 	}
 	else{
 		alarm(0);
@@ -52,7 +65,9 @@ int main(){
 	sigset_t mask, oldmask;
 	sigemptyset(&mask);
 	sigaddset(&mask, SIGALRM);
+	sigaddset(&mask, SIGUSR1);
 	signal(SIGALRM, alrm_handler);
+	signal(SIGUSR1, sigusr1_handler);
 	wiringPiISR(BUTTON_PIN, INT_EDGE_BOTH, button_click_handler);
 	wiringPiISR(MOTION_SENSOR_PIN, INT_EDGE_FALLING, take_picture);
 	
@@ -62,8 +77,14 @@ int main(){
 	
 	sigprocmask(SIG_BLOCK, &mask, &oldmask);
 	while(1){
-		while(!take_picture_check) {sigsuspend(&oldmask);}
-			take_picture();
-			take_picture_check = FALSE;
+		while(!link_frame_check && !show_video_check) {sigsuspend(&oldmask);}
+			if(link_frame_check){
+				link_frame();
+				link_frame_check 	= FALSE;
+			}
+			else{
+				show_video();
+				show_video_check 	= FALSE;
+			}
 	}
 }
