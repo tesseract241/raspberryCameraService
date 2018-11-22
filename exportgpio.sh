245657de851c79e6337559f95960a3fd9a9c862b
tesseract241 <endou.kenji5@gmail.com>
2018-11-22 13:08:17 +0100

    Typo

--- exportgpio.sh
+++ exportgpio.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BUTTON_PIN=27; MOTION_SENSOR_PIN=17
-gpio -export $BUTTON_PIN in
-gpio -export $MOTION_SENSOR_PIN in
+gpio export $BUTTON_PIN in
+gpio export $MOTION_SENSOR_PIN in
 gpio -g mode $BUTTON_PIN up
 gpio -g mode $MOTION_SENSOR_PIN tri
-gpio -edge $BUTTON_PIN both
-gpio -edge $MOTION_SENSOR_PIN rising
+gpio edge $BUTTON_PIN both
+gpio edge $MOTION_SENSOR_PIN rising
 exit 0
